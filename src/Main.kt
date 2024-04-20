f1940dc10acdac84238096fd5499d128932efe6f
devops-queue <dantseaxa+devops@gmail.com>
2024-04-20 16:51:54 +0800

    first commit testing interface and class

---
+++ src/Main.kt
@@ -0,0 +1,14 @@
+
+
+interface Animal {
+    fun makeVoice() {
+        print("<Animal voice>")
+    }
+}
+
+class Fox: Animal
+
+fun main() {
+    val fox = Fox()
+    fox.makeVoice() // <Animal voice>
+}
